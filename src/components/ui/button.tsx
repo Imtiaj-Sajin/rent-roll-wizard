7d8857aecc8a17f3ede598905415371110d0e264
Imtiaj-Sajin <imtiajsajin@gmail.com>
2026-02-05 11:24:01 +0600

    ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground ring-1 ring-transparent hover:ring-4 hover:ring-destructive/50 ",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
